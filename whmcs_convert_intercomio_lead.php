f69046e4ea78171ba75d325e83468eb5522f7039
Eric Baker <eric@ericbaker.me>
2017-05-05 00:39:18 -0400

    Makes sure file can't be accessed directly

--- whmcs_convert_intercomio_lead.php
+++ whmcs_convert_intercomio_lead.php
@@ -1,4 +1,8 @@
 <?php
+
+// Make sure the file cannot be accessed directly
+if ( ! defined( "WHMCS" ) ) die( "This file cannot be accessed directly" );
+
 // Set Intercom Access Token
 $access_token = '';
 $lead_email = '';
