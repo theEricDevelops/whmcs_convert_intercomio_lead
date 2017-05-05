35997cd8a55e2daa29c03ef74f9da7c184071c7c
Eric Baker <eric@ericbaker.me>
2017-05-04 23:38:49 -0400

    Intercom.io conversion functions independently.

--- hooks.php
+++ hooks.php
@@ -15,19 +15,22 @@ use Intercom\IntercomClient;
 
 // Set Intercom Access Token
 $access_token = '';
+$lead_email = 'eric@ericbaker.me';
 
 // Connect to Intercom
 $intercom = new IntercomClient( $access_token, null);
 
+// Get a lead by email
+function get_intercomio_lead_id_by_email($api_connection, $email) {
+	return $api_connection->leads->getLeads(['email' => $email])->contacts[0]->id;
+}
+
 // Do the stuff to find the lead and convert into a user
 function convert_lead_to_user($lead_email) {
-	// Get a Lead by Email
-	$lead = $intercom->leads->getLeads(['email' => $lead_email]);
-
 	// Convert the Lead to a User
 	$lead->convertLead([
 	  "contact" => [
-	    "user_id" => $lead['id']
+	    "user_id" => get_intercomio_lead_id_by_email($intercom, $lead_email);
 	  ],
 	  "user" => [
 	    "email" => $lead_email
@@ -36,5 +39,5 @@ function convert_lead_to_user($lead_email) {
 }
 
 // Now create the hook and call the function
-add_hook('ClientAdd', 1, convert_lead_to_user($vars['email']));
+//add_hook('ClientAdd', 1, convert_lead_to_user($vars['email']));
 
--- whmcs_convert_intercomio_lead.php
+++ whmcs_convert_intercomio_lead.php
@@ -1,14 +1,39 @@
 <?php
 
-function config() {
-    $configarray = array(
-    "name" => "WHMCS Convert Intercom.io Lead to User",
-    "description" => "This small WHMCS addon allows you the ability to convert an Intercom.io lead into a user by utilizing the ClientAdd hook from WHMCS.",
-    "version" => "1.0",
-    "author" => "Eric Baker <eric@ericbaker.me>",
-    "fields" => array(
-        "option1" => array ("Access Token" => "Option1", "Type" => "text", "Size" => "60",
-                              "Description" => "Your Intercom.io Access Token", "Default" => "", ),
-    ));
-    return $configarray;
+function whmcs_convert_intercomio_lead_config() {
+  $configarray = array(
+  "name" => "WHMCS Convert Intercom.io Lead to User",
+  "description" => "This small WHMCS addon allows you the ability to convert an Intercom.io lead into a user by utilizing the ClientAdd hook from WHMCS.",
+  "version" => "1.0",
+  "author" => "Eric Baker <eric@ericbaker.me>",
+  "fields" => array(
+      "option1" => array ("Access Token" => "Option1", "Type" => "text", "Size" => "60",
+                            "Description" => "Your Intercom.io Access Token", "Default" => "", ),
+  ));
+  return $configarray;
+}
+
+function whmcs_convert_intercomio_lead_activate() {
+  return array( 
+    'status' => 'success', 
+    'description' => 'Make sure to enter your Access Token in the settings page in order to start converting your new WHMCS clients within Intercom.io.'
+  );
+}
+
+function whmcs_convert_intercomio_lead_deactivate() {
+  // Remove the database entry for the access token.
+  $query = "DELETE FROM  `tbladdonmodules` WHERE  `module` =  'whmcs_convert_intercomio_lead'";
+  $result = full_query($query);
+
+  // Return result
+  return array(
+    'status' => 'success',
+    'description' => 'The module has been deactivated.'
+  );
+}
+
+function whmcs_convert_intercomio_lead_output($vars) {
+  $option1 = $vars['option1'];
+
+  
 }
